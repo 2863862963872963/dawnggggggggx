be32c59ae425f77ad4e27aea5bfd878c49f32538
2863862963872963 <119198880+2863862963872963@users.noreply.github.com>
2025-06-29 14:23:01 +0700

    Update Main script Of 99 Nights in the forest.lua

--- Main script Of 99 Nights in the forest.lua
+++ Main script Of 99 Nights in the forest.lua
@@ -1,1 +1,209 @@
-we
+local Compkiller = loadstring(game:HttpGet("https://raw.githubusercontent.com/4lpaca-pin/CompKiller/refs/heads/main/src/source.luau"))();
+local ConfigManager = Compkiller:ConfigManager({Directory = "K4yo Hub", Config = "Example-Configs"});
+local Module = loadstring(game:HttpGet("https://raw.githubusercontent.com/2863862963872963/dawnggggggggx/refs/heads/main/Compiler.inc"))();
+
+local Players = game:GetService("Players")
+local Player = Players.LocalPlayer
+
+local Class, translate, Configs, Funcs, Default = unpack(Module)
+local Game = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId)
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local RunService = game:GetService("RunService")
+
+local Remote = {
+	BridgeNet2 = ReplicatedStorage:WaitForChild("BridgeNet2"):WaitForChild("dataRemoteEvent")
+}
+local Config = {
+	["Farm Config"] = {
+		getgenv().AutoFarmLevel = nil,
+		getgenv().BossAllow = nil,
+	},
+}
+
+	
+// Quest Detection Logic
+local function GetQuest()
+	local Levels = Player.Data:GetAttribute("Levels")
+	local Quest = nil
+
+	if Levels >= 1 and Levels < 25 then 
+		Quest = "Quest 1"
+	elseif Levels >= 25 and Levels < 50 then 
+		Quest = "Quest 2"
+	elseif Levels >= 50 and Levels < 100 then 
+		Quest = "Quest 3"
+	elseif Levels >= 100 and Levels < 150 then 
+		Quest = "Quest 4"
+	elseif Levels >= 150 and Levels < 200 then 
+		Quest = "Quest 5"
+	elseif Levels >= 200 and Levels < 250 then 
+		Quest = "Quest 6"
+	elseif Levels >= 250 and Levels < 300 then 
+		Quest = "Quest 7"
+	elseif Levels >= 300 and Levels < 350 then 
+		Quest = "Quest 8"
+	elseif Levels >= 350 and Levels < 400 then 
+		Quest = "Quest 9"
+	elseif Levels >= 400 and Levels < 450 then 
+		Quest = "Quest 10"
+	elseif Levels >= 450 then 
+		Quest = "Quest 11"
+	end
+
+	return Quest
+end
+
+local function GetEnemyQuest()
+	local QuestN = GetQuest()
+	local Quests = Player.Quests
+
+	if QuestN and Quests:FindFirstChild(QuestN) then
+		local CurQuest = Quests:FindFirstChild(QuestN)
+		return CurQuest:GetAttribute("Objective")
+	end
+
+	return nil
+end
+
+local function AcceptQuest(QuestN)
+	Click = Class:Click()
+	isQuesting = Player.Quests:FindFirstChild(QuestN)
+    Quest = string.gsub(QuestN, " ", "")
+
+	if not isQuesting then
+		for i, v in pairs(workspace.Interact.Quest:GetChildren()) do
+		   if v:IsA("Model") and v.Name == Quest then
+				hrp = v:FindFirstChild("HumanoidRootPart")
+			    Class:tp(hrp.CFrame)
+			end
+		end
+	end
+end
+				
+			
+
+
+
+local function M1()
+	Remote.BridgeNet2:FireServer({{ "M1", CFrame.new(0, 0, 0), CFrame.new(0, 0, 0) }, "\4"})
+end
+		
+--// Auto Farm Logic (empty for now)
+function AutoFarmLevel()
+	local Quest = GetQuest()
+	local Enemy = GetEnemyQuest()
+
+	AcceptQuest(Quest)
+end
+
+local QuotesTable = {
+	"A ghost of a memory.",
+	"When the past whispers through the present.",
+	"Echoes of another time.",
+	"Moments suspended in time.",
+	"Faded Polaroids in the mind.",
+	"The scent of yesterday.",
+	"Where time folded and memory slipped through.",
+}
+local Quotes = QuotesTable[math.random(1, #QuotesTable)]
+
+--// Main UI Window
+local Window = Compkiller.new({
+	Name = "Aozora Hub",
+	Keybind = "LeftAlt",
+	Logo = "rbxassetid://120245531583106",
+	TextSize = 15,
+})
+
+--// Watermark Section
+local Watermark = Window:Watermark()
+local Quote = Watermark:AddText({Icon = "pencil", Text = Quotes})
+local Time = Watermark:AddText({Icon = "timer", Text = "TIME"})
+
+task.spawn(function()
+	while true do
+		task.wait()
+		Time:SetText(Compkiller:GetTimeNow())
+	end
+end)
+
+--// Farm Tabs
+local FarmTabs = Window:DrawContainerTab({
+	Name = "Farm",
+	Icon = "contact",
+})
+
+local FarmLevelTab = FarmTabs:DrawTab({
+	Name = "Level Farm",
+	Type = "Single",
+	EnableScrolling = true, 
+})
+
+local FarmLevelSection = FarmLevelTab:DrawSection({
+	Name = "Farm Level",
+	Position = 'left'	
+})
+
+FarmLevelSection:AddButton({
+	Name = "test",
+	Callback = function()
+		AutoFarmLevel()
+	end,
+})
+
+local AutoFarmLevelTog = FarmLevelSection:AddToggle({
+	Name = "Auto Farm Level",
+	Default = false,
+	Callback = function(State)
+		Config["Farm Config"].getgenv().AutoFarmLevel = State 
+	end,
+})
+
+local FarmLevelOption = AutoFarmLevelTog.Link:AddOption()
+FarmLevelOption:AddToggle({
+	Name = "Boss Allow ?",
+	Callback = function(state) 
+		Config["Farm Config"].getgenv().BossAllow = State 
+	end,
+})
+FarmLevelOption:AddToggle({
+	Name = "Safe Farm",
+	Callback = print
+})
+
+--// Theme Tab
+local ThemeTab = Window:DrawTab({
+	Icon = "paintbrush",
+	Name = "Themes",
+	Type = "Single"
+})
+
+ThemeTab:DrawSection({
+	Name = "UI Themes"
+}):AddDropdown({
+	Name = "Select Theme",
+	Default = "Default",
+	Values = {
+		"Default",
+		"Dark Green",
+		"Dark Blue",
+		"Purple Rose",
+		"Skeet"
+	},
+	Callback = function(v)
+		Compkiller:SetTheme(v)
+	end,
+})
+--// Config Tab
+local ConfigUI = Window:DrawConfig({
+	Name = "Config",
+	Icon = "folder",
+	Config = ConfigManager
+})
+ConfigUI:Init()
+
+
+
+
+
+
